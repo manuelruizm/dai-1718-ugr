40aecd1b7e49fee9aeaaba5c9332bfbec8ba7c6a
manuelruizm <ruizmaldonadomanuel@gmail.com>
2017-10-22 19:09:02 +0200

    P2-E3 - Hecho

--- Practica2/P2-Ejercicio3.py
+++ Practica2/P2-Ejercicio3.py
@@ -5,7 +5,7 @@
 #	DAI - Desarrollo de Aplicaciones para Internet
 #	Curso 2017-2018
 #
-#	Práctica 2: Ejercicio 2 - Soluciones
+#	Práctica 2: Ejercicio 3 - Soluciones
 #
 #------------------------------------------------------------------------------
 
@@ -18,7 +18,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('P2-E2.html')
+    return render_template('P2-E3-1.html')
+
+@app.route('/user')
+def user():
+    return render_template('P2-E3-2.html')
+
+@app.route('/user/<user>')
+def username(user):
+    return render_template('P2-E3-2.html', usuario=user)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Página no encontrada", 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
