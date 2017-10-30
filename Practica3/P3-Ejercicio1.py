7484bded37dedf5fa44d8fa21e1b88574b969e60
manuelruizm <ruizmaldonadomanuel@gmail.com>
2017-10-30 22:38:21 +0100

    Fichero .py de la P3

---
+++ Practica3/P3-Ejercicio1.py
@@ -0,0 +1,24 @@
+#------------------------------------------------------------------------------
+#
+#	Manuel Ruiz Maldonado
+#	
+#	DAI - Desarrollo de Aplicaciones para Internet
+#	Curso 2017-2018
+#
+#	Práctica 3: Ejercicio 1 - Soluciones
+#
+#------------------------------------------------------------------------------
+
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
